3f06e9f8a591f568d4281c3b6d0bd5b9dd6b3bd6
Mohammed Hameed <160359322+MohameedHameed@users.noreply.github.com>
2025-05-11 14:25:51 +0300

    cut the model var inside the predictimagefun

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
-
+model = None 
 # Load the model
-model = tf.keras.models.load_model('ClassicCNN_v1_28_0.914.keras')
 image_size = (32, 32)
 
 def preprocess_image_for_cnn(image_stream, image_size=(32, 32)):
@@ -55,6 +54,9 @@ labels = {
 
 @app.route('/predictImage', methods=['POST'])
 def predictImage():
+       global model
+    if model is None:
+        model = tf.keras.models.load_model('ClassicCNN_v1_28_0.914.keras')
     try:
         if 'image' not in request.files:
             return jsonify({
